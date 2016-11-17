4aca3c90733c0e0bee9e04b74a994b87f40ca7bc
seet61 <dmitry.arefyev@RST-BSS-041-NB.Megafon.ru>
2016-11-17 19:22:27 +0300

    Initial commit

---
+++ src/ru/MyPaint/MyPaint.java
@@ -0,0 +1,10 @@
+package ru.MyPaint;
+/**
+ * Наследование
+ * Created by dmitry.arefyev on 17.11.2016.
+ */
+public class MyPaint {
+    public static void main(String[] args) {
+        
+    }
+}
